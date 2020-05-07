99f416b0b2682241b6f2fce0c60aaa4064796df8
aecordova <ae_cordova@outlook.com>
2020-05-07 18:25:22 -0500

    Add factory for player objects

--- js/main.js
+++ js/main.js
@@ -2,38 +2,76 @@ const x = 'X';
 const o = 'O';
 
 const GameBoard = () => {
-  let positions = ["_","_","_","_","_","_","_","_","_"];
-
-  const setPos = (index, marker) => {
-    positions[index] = marker;
-  };
+  let positions = ['_', '_', '_', '_', '_', '_', '_', '_', '_'];
 
   const getBoard = () => positions;
-  
+
+  const posBlank = (index) => positions[index] === '_';
+
+  const setPos = (index, marker) => {
+    if (posBlank) {
+      positions[index] = marker;
+    }
+    return posBlank;
+  };
+
+
   const p = () => {
-    console.log("Current Board: [", ...positions,"]");
+    console.log('Current Board: [', ...positions, ']');
   };
 
   const clearBoard = () => {
-     positions = positions.map(() => "_");
+    positions = positions.map(() => '_');
   };
 
-  return { setPos, clearBoard, getBoard, p };
+
+  return {
+    setPos, clearBoard, getBoard, p, posBlank,
+  };
 };
 
-// board = GameBoard();
+const Player = (name, marker) => {
+  let score = 0;
+  const getScore = () => score;
+  const setScore = (newPoints) => {
+    score += newPoints;
+  };
 
-// board.setPos(5,x);
-// board.setPos(6,o);
-// board.p();
-// board.clearBoard();
+  return {
+    name, marker, getScore, setScore,
+  };
+};
+
+const Game = (player1, player2, board) => {
+  let gameOn= true;
+  while(gameOn){
+    
+  }
+  
+
+}
+
+// p1 = Player("Juan",x)
+// p1.name = "Angel"
+
+// console.log(p1.name);
+// console.log("marker",p1.marker);
+//  board = GameBoard();
+
+//  board.setPos(5,x);
+//  board.setPos(6,o);
+//  board.p();
+//  console.log(board.posBlank(5));
+//  console.log(board.posBlank(1));
+//  board.clearBoard();
+//  console.log(board.posBlank(5));
+
 // board.p();
 
 // const Game = (board, player1, player2) => {
-  
+
 // }
 
 // const Player = (name, marker) => {
 
 // }
-
