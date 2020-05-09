7c408accc6ad4e7555d2119ea6eb13f59c112af1
aecordova <ae_cordova@outlook.com>
2020-05-09 18:46:17 -0500

    Add formating for new game buton and glowing effect for on hover

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 // eslint-disable-next-line import/extensions
 import * as gameLogic from './game_logic.js';
+
+const newGameBtn = document.getElementById('new-game-btn');
+const newGameBox = document.getElementById('new-game-box');
+const playerBox = document.getElementById('player-box');
+const board = document.getElementById('board');
+
+newGameBtn.addEventListener('click', () => {
+  newGameBox.classList.toggle('d-none');
+  board.classList.toggle('d-none');
+  playerBox.classList.toggle('hidden');
+});
