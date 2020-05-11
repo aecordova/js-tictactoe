2247fb21818d9184049239ec0fd4f78acf3121fa
Treasure Kabareebe <treasurecalm@gmail.com>
2020-05-12 02:01:44 +0300

    Add logic for restarting the game

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ import * as gameLogic from './game_logic.js';
 
 const newGameBtn = document.getElementById('new-game-btn');
 const newGameBox = document.getElementById('new-game-box');
+const replayGameBtn = document.getElementById('replay-game-btn');
+const winnerBanner = document.querySelector('.winner-banner');
 const playerBox = document.getElementById('player-box');
 const board = document.getElementById('board');
 const cells = document.querySelectorAll('.board-cell')
@@ -16,17 +18,42 @@ newGameBtn.addEventListener('click', () => {
   newGameBox.classList.toggle('d-none');
   board.classList.toggle('d-none');
   playerBox.classList.toggle('hidden');
+  clearBoard();
+});
+
+replayGameBtn.addEventListener('click', () => {
+  game = gameLogic.Game(player1, player2);
+  winnerBanner.classList.add('d-none');
+  //players[0].classList.add('highlight');
+  clearBoard();
 });
 
 cells.forEach((cell, i) => {
   cell.addEventListener('click', (e) => {
     if (game.isOn()){
       cell.classList.add(`${game.mark(i)}`);
-
       players.forEach((player) => {
         player.classList.toggle('highlight');
       });
     }
   });
 });
+
+const clearBoard = () => {
+  cells.forEach((cell) => {
+    
+    if (cell.classList.value.includes('x')){
+      cell.classList.remove('x');
+    }
+    if (cell.classList.value.includes('o')){
+      cell.classList.remove('o');
+    }
+    // players.forEach((player) => {
+    //   player.classList.remove('highlight');
+    // });
+
+  });
+}
+
+
 
